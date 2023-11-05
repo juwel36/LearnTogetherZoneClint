6ff5ba8484ee25eb77cf4981f65925f0c3864d73
juwel36 <nothingsomthing3@gmail.com>
2023-11-06 00:18:13 +0600

    design banner section and implement this in home page

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,30 @@
+import img from '../assets/bNNE_20231105_235247.png'
+
+const Banner = () => {
+  return (
+    <div className='bg-white border-2 border-x-teal-800 rounded-lg  m-5 my-9'>
+      <div className='flex flex-col lg:flex-row justify-center items-center '>
+
+<div>
+  <p className='text-black py-1'>Assignment Adventure Awaits</p>
+<h1 className='text-teal-800 text-5xl font-bold '> Friends + Learning = Success </h1>
+<p className='text-gray-500 text-xs'>Embark on an exciting assignment adventure with friends. <br /> Together, we learn.</p>
+
+</div>
+
+
+<div className='p-3 py-14'>
+  <img className='w-96' src={img} alt="" />
+</div>
+
+
+      </div>
+
+
+
+
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,19 @@
+import Banner from "../../Components/Banner";
 import Footer from "../../Components/Navbar/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
 
 
 const Home = () => {
   return (
-    <div>
+  <div className="bg-white">
+      <div className="max-w-6xl mx-auto">
       <Navbar></Navbar>
+<Banner></Banner>
 
 <Footer></Footer>
 
     </div>
+  </div>
   );
 };
 
