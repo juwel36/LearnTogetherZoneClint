5fdb131eb8f1eb6916702ef0517cb13e21639053
juwel36 <nothingsomthing3@gmail.com>
2023-11-05 22:36:25 +0600

    implement  Assignment deleting button for delete assignment also remove it from ui

--- src/Pages/myAssignment/MyAssignment.jsx
+++ src/Pages/myAssignment/MyAssignment.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 
 const MyAssignment = () => {
@@ -33,6 +34,30 @@ const MyAssignment = () => {
 
   const filteredData = data.filter((item) => item.status === statusFilter);
 
+  const handleremove = (id) =>{
+    console.log(id);
+  
+   
+  fetch(`http://localhost:5000/create/${id}`,{
+  method:'DELETE',
+  })
+  .then(res=> res.json())
+  .then(data=>{
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: ' Delete succesfully',
+      showConfirmButton: false,
+      timer: 2000
+    })
+    console.log(data);
+  
+    setData((prevUser) => prevUser.filter((item) => item._id !== id));
+  })
+  }
+
+
+
   return (
    <div className="bg-white">
      <div className="max-w-6xl mx-auto">
@@ -46,6 +71,7 @@ const MyAssignment = () => {
               <h2 className="card-title"> {item.title} </h2>
               <p>Click the button to listen on Spotify app.</p>
               <div className="card-actions justify-end">
+                <button onClick={()=>handleremove(item._id)} className="btn bg-sky-900 text-white"> Delete 🚮 </button>
                 <button className="btn bg-sky-900 text-white">{item.status}</button>
               </div>
             </div>
